c803e5b6899727357b026024493dd14b89f0327d
Aidan Feldman <aidan.feldman@gsa.gov>
2016-12-20 11:02:08 -0500

    switch to using a mock of the RouteManager

--- broker/broker_misc_test.go
+++ broker/broker_misc_test.go
@@ -11,15 +11,16 @@ import (
 
 	"github.com/18F/cf-cdn-service-broker/broker"
 	"github.com/18F/cf-cdn-service-broker/models"
+	"github.com/18F/cf-cdn-service-broker/models/mocks"
 )
 
 func TestLastOperationMissing(t *testing.T) {
+	manager := mocks.RouteManagerIface{}
+	manager.On("Get", "").Return(models.Route{}, errors.New("not found"))
 	b := broker.CdnServiceBroker{
-		Manager: &FakeRouteManager{
-			RouteGet: models.Route{},
-			ErrorGet: errors.New(""),
-		},
+		Manager: &manager,
 	}
+
 	operation, err := b.LastOperation("")
 	assert.Equal(t, operation.State, brokerapi.Failed)
 	assert.Equal(t, operation.Description, "Service instance not found")
@@ -27,51 +28,57 @@ func TestLastOperationMissing(t *testing.T) {
 }
 
 func TestLastOperationSucceeded(t *testing.T) {
-	b := broker.CdnServiceBroker{
-		Manager: &FakeRouteManager{
-			RouteGet: models.Route{
-				State:          models.Provisioned,
-				DomainExternal: "cdn.cloud.gov",
-				Origin:         "cdn.apps.cloud.gov",
-			},
-			ErrorGet: nil,
-		},
+	manager := mocks.RouteManagerIface{}
+	route := models.Route{
+		State:          models.Provisioned,
+		DomainExternal: "cdn.cloud.gov",
+		Origin:         "cdn.apps.cloud.gov",
 	}
-	operation, err := b.LastOperation("")
+	manager.On("Get", "123").Return(route, nil)
+	manager.On("Update", route).Return(nil)
+	b := broker.CdnServiceBroker{
+		Manager: &manager,
+	}
+
+	operation, err := b.LastOperation("123")
 	assert.Equal(t, operation.State, brokerapi.Succeeded)
 	assert.Equal(t, operation.Description, "Service instance provisioned [cdn.cloud.gov => cdn.apps.cloud.gov]")
 	assert.Nil(t, err)
 }
 
 func TestLastOperationProvisioning(t *testing.T) {
-	b := broker.CdnServiceBroker{
-		Manager: &FakeRouteManager{
-			RouteGet: models.Route{
-				State:          models.Provisioning,
-				DomainExternal: "cdn.cloud.gov",
-				Origin:         "cdn.apps.cloud.gov",
-			},
-			ErrorGet: nil,
-		},
+	manager := mocks.RouteManagerIface{}
+	route := models.Route{
+		State:          models.Provisioning,
+		DomainExternal: "cdn.cloud.gov",
+		Origin:         "cdn.apps.cloud.gov",
 	}
-	operation, err := b.LastOperation("")
+	manager.On("Get", "123").Return(route, nil)
+	manager.On("Update", route).Return(nil)
+	b := broker.CdnServiceBroker{
+		Manager: &manager,
+	}
+
+	operation, err := b.LastOperation("123")
 	assert.Equal(t, operation.State, brokerapi.InProgress)
 	assert.True(t, strings.Contains(operation.Description, "Provisioning in progress [cdn.cloud.gov => cdn.apps.cloud.gov]"))
 	assert.Nil(t, err)
 }
 
 func TestLastOperationDeprovisioning(t *testing.T) {
-	b := broker.CdnServiceBroker{
-		Manager: &FakeRouteManager{
-			RouteGet: models.Route{
-				State:          models.Deprovisioning,
-				DomainExternal: "cdn.cloud.gov",
-				Origin:         "cdn.apps.cloud.gov",
-			},
-			ErrorGet: nil,
-		},
+	manager := mocks.RouteManagerIface{}
+	route := models.Route{
+		State:          models.Deprovisioning,
+		DomainExternal: "cdn.cloud.gov",
+		Origin:         "cdn.apps.cloud.gov",
 	}
-	operation, err := b.LastOperation("")
+	manager.On("Get", "123").Return(route, nil)
+	manager.On("Update", route).Return(nil)
+	b := broker.CdnServiceBroker{
+		Manager: &manager,
+	}
+
+	operation, err := b.LastOperation("123")
 	assert.Equal(t, operation.State, brokerapi.InProgress)
 	assert.Equal(t, operation.Description, "Deprovisioning in progress [cdn.cloud.gov => cdn.apps.cloud.gov]")
 	assert.Nil(t, err)
