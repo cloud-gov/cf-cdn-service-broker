d6685688749abc8d6ad57a12c8ec5333ba3d7d7f
Aidan Feldman <aidan.feldman@gsa.gov>
2016-12-19 19:43:25 -0500

    split broker tests to separate files

---
+++ broker/fake_route_manager_test.go
@@ -0,0 +1,34 @@
+package broker_test
+
+import "github.com/18F/cf-cdn-service-broker/models"
+
+type FakeRouteManager struct {
+	RouteCreate models.Route
+	ErrorCreate error
+	RouteGet    models.Route
+	ErrorGet    error
+}
+
+func (m *FakeRouteManager) Create(instanceId, domain, origin, path string, insecureOrigin bool, tags map[string]string) (models.Route, error) {
+	return m.RouteCreate, m.ErrorCreate
+}
+
+func (m *FakeRouteManager) Get(instanceId string) (models.Route, error) {
+	return m.RouteGet, m.ErrorGet
+}
+
+func (m *FakeRouteManager) Update(models.Route) error {
+	return nil
+}
+
+func (m *FakeRouteManager) Disable(models.Route) error {
+	return nil
+}
+
+func (m *FakeRouteManager) Renew(models.Route) error {
+	return nil
+}
+
+func (m *FakeRouteManager) RenewAll() {
+	return
+}
