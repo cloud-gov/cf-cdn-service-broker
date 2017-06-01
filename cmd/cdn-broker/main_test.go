bddf848786acba7d667c4271ed88cd6b68aa2060
Dan Carley <dan.carley@gmail.com>
2017-06-01 15:14:16 +0100

    Fix tests for HTTP handlers
    The refactoring in abcb000 caused the tests to fail with:
    
        Failed to compile cdn-broker:
    
        # github.com/18F/cf-cdn-service-broker/cmd/cdn-broker
        cmd/cdn-broker/main_test.go:14: undefined: buildHTTPHandler
    
    Fix them by using the new `func bindHTTPHandlers` instead.

--- cmd/cdn-broker/main_test.go
+++ cmd/cdn-broker/main_test.go
@@ -7,15 +7,17 @@ import (
 
 	"code.cloudfoundry.org/lager"
 	"github.com/18F/cf-cdn-service-broker/broker"
+	"github.com/18F/cf-cdn-service-broker/config"
 	"github.com/pivotal-cf/brokerapi"
 )
 
-func TestBuildHTTPHandler(t *testing.T) {
-	handler := buildHTTPHandler(
+func TestHTTPHandler(t *testing.T) {
+	brokerAPI := brokerapi.New(
 		&broker.CdnServiceBroker{},
 		lager.NewLogger("main.test"),
 		brokerapi.BrokerCredentials{},
 	)
+	handler := bindHTTPHandlers(brokerAPI, config.Settings{})
 	req, err := http.NewRequest("GET", "http://example.com/healthcheck/http", nil)
 	if err != nil {
 		t.Error("Building new HTTP request: error should not have occurred")
