1f1bd5b7644a19428f6758f36b4b6818a3d29862
Joshua Carp <jm.carp@gmail.com>
2017-11-16 11:46:24 -0500

    Restore TLS 1 and deprecate DES cipher.
    h/t @konklone

--- utils/cloudfront.go
+++ utils/cloudfront.go
@@ -136,8 +136,9 @@ func (d *Distribution) fillDistributionConfig(config *cloudfront.DistributionCon
 					HTTPSPort:            aws.Int64(443),
 					OriginProtocolPolicy: getOriginProtocolPolicy(insecureOrigin),
 					OriginSslProtocols: &cloudfront.OriginSslProtocols{
-						Quantity: aws.Int64(2),
+						Quantity: aws.Int64(3),
 						Items: []*string{
+							aws.String("TLSv1.0"),
 							aws.String("TLSv1.1"),
 							aws.String("TLSv1.2"),
 						},
@@ -274,7 +275,7 @@ func (d *Distribution) SetCertificate(distId, certId string) error {
 	DistributionConfig.ViewerCertificate.IAMCertificateId = aws.String(certId)
 	DistributionConfig.ViewerCertificate.CertificateSource = aws.String("iam")
 	DistributionConfig.ViewerCertificate.SSLSupportMethod = aws.String("sni-only")
-	DistributionConfig.ViewerCertificate.MinimumProtocolVersion = aws.String("TLSv1.1_2016")
+	DistributionConfig.ViewerCertificate.MinimumProtocolVersion = aws.String("TLSv1_2016")
 	DistributionConfig.ViewerCertificate.CloudFrontDefaultCertificate = aws.Bool(false)
 
 	_, err = d.Service.UpdateDistribution(&cloudfront.UpdateDistributionInput{
