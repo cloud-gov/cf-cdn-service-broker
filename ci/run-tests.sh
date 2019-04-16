34baf51c74952df8134150840c34348df683a0d9
Mike Lloyd <mike.lloyd@gsa.gov>
2019-04-16 10:12:43 -0600

    updated golang image, updated test commands.
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,13 +2,8 @@
 
 set -eux
 
-export GOPATH=$(pwd)/gopath
-export PATH=${PATH}:${GOPATH}/bin
-mkdir -p ${GOPATH}/bin
-
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-
-pushd gopath/src/github.com/18F/cf-cdn-service-broker
-  dep ensure
-  go test $(go list ./... | grep -v /vendor/)
+pushd cf-cdn-service-broker
+  go mod download
+  go mod vendor
+  go test -v ./...
 popd
