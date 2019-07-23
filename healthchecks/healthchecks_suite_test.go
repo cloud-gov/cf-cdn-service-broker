alphagov/paas-cf-cdn-service-broker

add no-op test for letsencrypt healthcheck
in later commits we can change how this works

Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

healthchecks/healthchecks_suite_test.go
@@ -1,0 +1,13 @@
+package healthchecks_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestHealthchecks(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Healthchecks Suite")
+}

cloud-gov/cf-cdn-service-broker

healthchecks/healthchecks_suite_test.go
@@ -1,0 +1,13 @@
+package healthchecks
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestHealthchecks(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Healthchecks Suite")
+}