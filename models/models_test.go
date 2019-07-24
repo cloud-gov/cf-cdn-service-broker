d25cc277a7942b83ac264bf6456bf69cbbb0c4bd
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-07-24 09:08:24 +0100

    Fix typo in test comment
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- models/models_test.go
+++ models/models_test.go
@@ -209,7 +209,7 @@ var _ = Describe("Models", func() {
 		mui.Settings = settings
 		mui.Service = fakeiam
 
-		// expect the orphaned certs to be deleted
+		// expect the orphaned certs to fail to be deleted
 		mui.On("DeleteCertificate", "some-orphaned-cert").Return(errors.New("DeleteCertificate error"))
 
 		m := models.NewManager(
