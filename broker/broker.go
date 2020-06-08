865017ffcce26d92cfcb94b57fea0cbc913958d4
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-06-08 15:33:33 +0100

    ACM revert: Revert "Remove dangerous automation...
    ...timeout codepath"
    
    This reverts commit c0f90220005687e92fd4fa92160ce322445eff7e.
    
    We are reverting some commits since we diverged in the ACM branch

--- broker/broker.go
+++ broker/broker.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"time"
 
 	"code.cloudfoundry.org/lager"
 	"github.com/pivotal-cf/brokerapi"
@@ -197,6 +198,30 @@ func (b *CdnServiceBroker) LastOperation(
 
 	switch route.State {
 	case models.Provisioning:
+		if route.CreatedAt.Before(time.Now().Add(-24 * time.Hour)) {
+			err = b.manager.Disable(route)
+			if err != nil {
+				lsession.Error("unable-to-expire-unprovisioned-instance", err, lager.Data{
+					"domain":    route.DomainExternal,
+					"state":     route.State,
+					"createdAt": route.CreatedAt,
+				})
+				return brokerapi.LastOperation{
+					State:       brokerapi.Failed,
+					Description: "Couldn't verify in 24h time slot. Self-healing has failed. Please contact support.",
+				}, nil
+			}
+
+			lsession.Info("expiring-unprovisioned-instance", lager.Data{
+				"domain":    route.DomainExternal,
+				"state":     route.State,
+				"createdAt": route.CreatedAt,
+			})
+			return brokerapi.LastOperation{
+				State:       brokerapi.InProgress,
+				Description: "Couldn't verify in 24h time slot. Expiring instance initialisation.",
+			}, nil
+		}
 		instructions, err := b.manager.GetDNSInstructions(route)
 		if err != nil {
 			lsession.Error("get-dns-instructions-err", err, lager.Data{
@@ -271,6 +296,9 @@ func (b *CdnServiceBroker) LastOperation(
 		}, nil
 	default:
 		description := "Service instance stuck in unmanagable state."
+		if route.CreatedAt.Before(time.Now().Add(-24 * time.Hour)) {
+			description = fmt.Sprintf("Couldn't verify in 24h time slot. %s", description)
+		}
 		lsession.Info("unmanagable-state", lager.Data{
 			"domain":      route.DomainExternal,
 			"state":       route.State,
